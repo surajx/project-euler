e28c8152acba0e912738ea2d3f04df0b6795c3ef
Suraj Narayanan <surajx@gmail.com>
2014-12-30 17:31:05 +0530

    removed default way

--- 2/aMilliionEvenFibs.js
+++ 2/aMilliionEvenFibs.js
@@ -24,24 +24,3 @@ var problem2 = function(){
 }
 
 problem2();
-
-var usingDefault = function(){
-  var array = new Array(1, 2);
-
-  var sum = 0;
-  var t = process.hrtime()
-  for(i = 1; array[i] < 4000000; i++) {
-
-    array[i + 1] = array[i - 1] + array[i];
-
-    if(array[i] % 2 == 0) {
-      sum += array[i];
-    }
-
-  }
-  var t1 = process.hrtime(t);
-  console.log("%s %d seconds and %d nanoseconds", "Fib the Normal way", t1[0], t1[1]);
-  console.log(sum);
-}
-
-usingDefault();
