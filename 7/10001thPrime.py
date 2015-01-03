d284a96432b1f994a939fa87a5225936568977af
Suraj Narayanan <surajx@gmail.com>
2015-01-03 19:37:53 +0530

    Solution to problem 7

---
+++ 7/10001thPrime.py
@@ -0,0 +1,23 @@
+import math
+
+def is_prime(n):
+  if n <= 3:
+    return n >= 2
+  if n % 2 == 0 or n % 3 == 0:
+    return False
+  for i in range(5, int(n ** 0.5) + 1, 6):
+    if n % i == 0 or n % (i + 2) == 0:
+      return False
+  return True
+
+def get_nth_prime(n):
+  prime_cnt = 1
+  cur_num = 3
+  while True:
+    if is_prime(cur_num):
+      prime_cnt+=1
+      if prime_cnt==n:
+        return cur_num
+    cur_num+=2
+
+print get_nth_prime(10001)
